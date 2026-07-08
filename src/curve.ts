f8219b8e5c25ca2a5fff57f1413651ee314db747
Neykoor <cafecafirexos@gmail.com>
2026-07-08 11:57:38 -0700

    Update curve.ts

--- src/curve.ts
+++ src/curve.ts
@@ -1,8 +1,8 @@
 import { generateKeyPairSync, diffieHellman } from 'node:crypto';
 
 export interface KeyPair {
-    publicKey: Buffer;
-    privateKey: Buffer;
+    pubKey: Buffer;
+    privKey: Buffer;
 }
 
 const SPKI_HEADER = Buffer.from([0x30, 0x2a, 0x30, 0x05, 0x06, 0x03, 0x2b, 0x65, 0x6e, 0x03, 0x21, 0x00]);
@@ -14,14 +14,14 @@ export function generateKeyPair(): KeyPair {
         privateKeyEncoding: { type: 'pkcs8', format: 'der' }
     });
     return {
-        publicKey: publicKey.subarray(SPKI_HEADER.length),
-        privateKey: privateKey.subarray(PKCS8_HEADER.length)
+        pubKey: publicKey.subarray(SPKI_HEADER.length),
+        privKey: privateKey.subarray(PKCS8_HEADER.length)
     };
 }
 
-export function calculateAgreement(publicKey: Buffer, privateKey: Buffer): Buffer {
+export function calculateAgreement(pubKey: Buffer, privKey: Buffer): Buffer {
     return diffieHellman({
-        privateKey: Buffer.concat([PKCS8_HEADER, privateKey]),
-        publicKey: Buffer.concat([SPKI_HEADER, publicKey])
+        privateKey: Buffer.concat([PKCS8_HEADER, privKey]),
+        publicKey: Buffer.concat([SPKI_HEADER, pubKey])
     });
 }
