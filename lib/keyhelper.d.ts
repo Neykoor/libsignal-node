247bf69f309813bb0606c9aae62012aa0db41c39
Neykoor <cafecafirexos@gmail.com>
2026-07-13 03:22:57 -0700

    Update keyhelper.d.ts

--- lib/keyhelper.d.ts
+++ lib/keyhelper.d.ts
@@ -4,11 +4,14 @@ export interface SignedPreKey {
     keyId: number;
     keyPair: KeyPair;
     signature: Buffer;
+    createdAt: number;
 }
 export interface PreKey {
     keyId: number;
     keyPair: KeyPair;
 }
+export declare const DEFAULT_SIGNED_PRE_KEY_MAX_AGE_MS: number;
+export declare function shouldRotateSignedPreKey(createdAt: number, maxAgeMs?: number): boolean;
 export declare const generateIdentityKeyPair: typeof curve.generateKeyPair;
 export declare function generateRegistrationId(): number;
 export declare function generateSignedPreKey(identityKeyPair: KeyPair, signedKeyId: number): SignedPreKey;
