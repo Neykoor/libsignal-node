88180261e02a4e1859e3546ff6ea9a6d3a4186e6
Neykoor <cafecafirexos@gmail.com>
2026-07-13 17:28:12 -0700

    Delete src directory

--- src/protobufs.ts
+++
@@ -1,6 +0,0 @@
-import root, { textsecure } from './whisper-text-protocol.js'
-
-export const WhisperMessage = root.textsecure.WhisperMessage
-export const PreKeyWhisperMessage = root.textsecure.PreKeyWhisperMessage
-export type WhisperMessage = textsecure.WhisperMessage
-export type PreKeyWhisperMessage = textsecure.PreKeyWhisperMessage
