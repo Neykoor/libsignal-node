8e23a66835755b5db7905e41fdc230f78f9bd848
Neykoor <cafecafirexos@gmail.com>
2026-07-14 17:39:57 -0700

    Update protocol-address.ts

--- src/protocol-address.ts
+++ src/protocol-address.ts
@@ -1,53 +1,53 @@
 export class ProtocolAddress {
-	public id: string
-	public deviceId: number
+  public id: string
+  public deviceId: number
 
-	static from(encodedAddress: string): ProtocolAddress {
-		if (typeof encodedAddress !== 'string') {
-			throw new Error('Invalid address encoding')
-		}
+  static from(encodedAddress: string): ProtocolAddress {
+    if (typeof encodedAddress !== "string") {
+      throw new Error("Invalid address encoding")
+    }
 
-		const sep = encodedAddress.lastIndexOf('.')
-		if (sep === -1) {
-			throw new Error('Invalid address encoding')
-		}
+    const sep = encodedAddress.lastIndexOf(".")
+    if (sep === -1) {
+      throw new Error("Invalid address encoding")
+    }
 
-		const id = encodedAddress.slice(0, sep)
-		const deviceIdPart = encodedAddress.slice(sep + 1)
-		if (!/^\d+$/.test(deviceIdPart)) {
-			throw new Error('Invalid address encoding')
-		}
+    const id = encodedAddress.slice(0, sep)
+    const deviceIdPart = encodedAddress.slice(sep + 1)
+    if (!/^\d+$/.test(deviceIdPart)) {
+      throw new Error("Invalid address encoding")
+    }
 
-		return new ProtocolAddress(id, parseInt(deviceIdPart, 10))
-	}
+    return new ProtocolAddress(id, parseInt(deviceIdPart, 10))
+  }
 
-	constructor(id: string, deviceId: number) {
-		if (typeof id !== 'string') {
-			throw new TypeError('id required for addr')
-		}
+  constructor(id: string, deviceId: number) {
+    if (typeof id !== "string") {
+      throw new TypeError("id required for addr")
+    }
 
-		if (id.indexOf('.') !== -1) {
-			throw new TypeError('encoded addr detected')
-		}
+    if (id.indexOf(".") !== -1) {
+      throw new TypeError("encoded addr detected")
+    }
 
-		this.id = id
+    this.id = id
 
-		if (typeof deviceId !== 'number') {
-			throw new TypeError('number required for deviceId')
-		}
+    if (typeof deviceId !== "number") {
+      throw new TypeError("number required for deviceId")
+    }
 
-		this.deviceId = deviceId
-	}
+    this.deviceId = deviceId
+  }
 
-	toString(): string {
-		return `${this.id}.${this.deviceId}`
-	}
+  toString(): string {
+    return `${this.id}.${this.deviceId}`
+  }
 
-	is(other: unknown): boolean {
-		if (!(other instanceof ProtocolAddress)) {
-			return false
-		}
+  is(other: unknown): boolean {
+    if (!(other instanceof ProtocolAddress)) {
+      return false
+    }
 
-		return other.id === this.id && other.deviceId === this.deviceId
-	}
+    return other.id === this.id && other.deviceId === this.deviceId
+  }
 }
