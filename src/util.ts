1b42223f4bea1923ce107dbe70f2b36ff235816b
Neykoor <cafecafirexos@gmail.com>
2026-07-14 17:43:46 -0700

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,11 @@
 export function wipeBuffer(buffer: Buffer | undefined | null): void {
-	if (buffer && buffer.byteLength > 0) {
-		buffer.fill(0)
-	}
+  if (buffer && buffer.byteLength > 0) {
+    buffer.fill(0)
+  }
 }
 
 export function wipeBuffers(...buffers: Array<Buffer | undefined | null>): void {
-	for (const buffer of buffers) {
-		wipeBuffer(buffer)
-	}
+  for (const buffer of buffers) {
+    wipeBuffer(buffer)
+  }
 }
