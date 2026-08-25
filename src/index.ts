93279631d95d40e3740bb4a720babb3c10821b65
Neykoor <cafecafirexos@gmail.com>
2026-08-24 23:18:31 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export type { SignalLogger } from "./logger"
 export { wipeBuffer, wipeBuffers } from "./util"
 export * from "./errors"
 export type { KeyPair } from "./curve"
-export type { SignalStorage, DeviceKeyBundle, IncomingPreKeyMessage, EncryptedMessage } from "./types"
+export type { SignalStorage, SenderKeyStore, DeviceKeyBundle, IncomingPreKeyMessage, EncryptedMessage } from "./types"
 export type {
   Chain,
   ChainKey,
@@ -25,3 +25,18 @@ export type {
   PendingPreKey,
   SerializedSessionEntry
 } from "./session-record"
+
+
+export * as groupKeyHelper from "./group-keyhelper"
+export { CiphertextMessage } from "./ciphertext-message"
+export { GroupCipher } from "./group-cipher"
+export { GroupSessionBuilder } from "./group-session-builder"
+export { SenderChainKey } from "./sender-chain-key"
+export { SenderKeyDistributionMessage } from "./sender-key-distribution-message"
+export { SenderKeyMessage } from "./sender-key-message"
+export { SenderKeyName } from "./sender-key-name"
+export { SenderKeyRecord } from "./sender-key-record"
+export { SenderKeyState } from "./sender-key-state"
+export type { SerializedSenderKeyRecord } from "./sender-key-record"
+export type { SerializedSenderKeyState } from "./sender-key-state"
+export { SenderMessageKey } from "./sender-message-key"
